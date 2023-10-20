0a3d2c03b8af0836feacadd74b46d6ce57bd5843
deocarri <146883880+deocarri@users.noreply.github.com>
2023-10-19 23:53:53 -0700

    Session 2 Prompt for Coding Journal 1

---
+++ Code_Journal_1_Prompt_2.py
@@ -0,0 +1,11 @@
+def main():
+	x = 6.0 
+	y = 4.0
+	a = 6
+	b = 4
+	print('x + y =', x+y) #sum of two floating numbers
+	print('x - y =', a-b) #difference of two integers
+	print('x * y =', x*b) #product of floating and integer
+
+if __name__ == '__main__':
+	main()
